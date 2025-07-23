5ca0b71850ce468f8d12f2c900dd53c8ccb10580
vengalarao-2000 <92215918+vengalarao-2000@users.noreply.github.com>
2025-07-23 19:14:28 +0000

    Invoke createTour() from Application.java

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
@@ -11,6 +12,11 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+
+        // Pull the TourManagementService object
+        TourManagementService tourManagementService = context.getBean(TourManagementService.class);
+        tourManagementService.createTour("Zoo Tour", 500, true);
+
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
